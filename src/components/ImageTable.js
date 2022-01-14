001090ed520b1054d5354e068ee56f31363f8dde
KanG98 <kylexue@Yankangs-MBP.fios-router.home>
2022-01-14 15:19:03 -0500

    added image grid css

--- src/components/ImageTable.js
+++ src/components/ImageTable.js
@@ -1,21 +1,32 @@
 import React from "react"
 import ImageCell from "./ImageCell"
+import "./../App.css"
 
 export default function ImageTable(props){
     return(
         <div style={styles.container}>
             {props.imgArr === [] ? <p style={{color: "white"}}>empty</p> : 
-                props.imgArr.map((data) => <><ImageCell key={data['id']} data={data}/></>)}
+            <div className="row">
+                    <div className="column">
+                        {props.imgArr.slice(0, props.imgArr.length/3).map((data) => <ImageCell key={data['id']} className="gif" data={data}/>)}
+                    </div>
+                    <div className="column">
+                        {props.imgArr.slice(props.imgArr.length/3, props.imgArr.length*2/3).map((data) => <ImageCell key={data['id']} className="gif" data={data}/>)} 
+                    </div>
+                    <div className="column">
+                        {props.imgArr.slice(props.imgArr.length * 2/3).map((data) => <ImageCell key={data['id']} className="gif" data={data}/>)} 
+                    </div>
+            </div>
+            }
         </div>
     )
 }
 
 const styles = {
     container:{
-        width: "600px",
-        justifyContent: "center",
-        backgroundColor: "red",
-        margin: "0 auto"
+        boxSizing: "border-box",
+        width: "620px",
+        margin: "0 auto",
+        marginTop: "20px"
     }
-
 }
