5b9c3e648c32f61dcea394dc7e57bc7e20b2c57a
KanG98 <kylexue@Yankangs-MBP.fios-router.home>
2022-01-17 01:52:58 -0500

    fixed git page http/https issue

--- src/scripts/queryRandomImg.js
+++ src/scripts/queryRandomImg.js
@@ -1,6 +1,6 @@
 export default async function queryRandomImg(imgArr, setImgArr, apiKey){
-    
-    return await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+
+    return await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
         .then(res => res.json())
         .then(res => {
             setImgArr([res["data"]])
--- src/scripts/queryTrendingImg.js
+++ src/scripts/queryTrendingImg.js
@@ -2,7 +2,7 @@ import sortImgSize from "./sortImgSize"
 
 export default async function queryTrendingImg(imgArr, setImgArr, apiKey){
     
-    return await fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`)
+    return await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`)
         .then(res => res.json())
         .then(res => {
             // sortImgSize(res["data"], setImgArr)
